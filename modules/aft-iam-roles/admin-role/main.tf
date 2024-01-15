eee257366cb7df57eefea2ed2ff3dae7bbf519ff
tapuhi <avi.apelbaum@psifas.org.il>
2024-01-15 11:11:39 +0200

    initial-commit

---
+++ modules/aft-iam-roles/admin-role/main.tf
@@ -0,0 +1,11 @@
+# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.11.0, < 6.0.0"
+    }
+  }
+}
---
+++ versions.tf
@@ -0,0 +1,14 @@
+# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source                = "hashicorp/aws"
+      version               = ">= 5.11.0, < 6.0.0"
+      configuration_aliases = [aws.ct_management, aws.log_archive, aws.audit, aws.aft_management, aws.tf_backend_secondary_region]
+    }
+  }
+}
